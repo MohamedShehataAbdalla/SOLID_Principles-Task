86f5856029de42cda9e7ab3190fc0f2d097e00eb
MohamedShehataAbdalla <moh.shehata143@gmail.com>
2022-10-27 01:26:24 +0200

    The Single Responsibility Principle (SRP)

---
+++ src/classes/SingleResponsibilityPrinciple/Correct/Keeper.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace src\classes\SingleResponsibilityPrinciple\Correct;
+
+class Keeper
+{
+
+}
---
+++ src/classes/SingleResponsibilityPrinciple/Incorrect/Match.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace src\classes\SingleResponsibilityPrinciple\Incorrect;
+
+class Match
+{
+    public function Attack(){
+
+    }
+    public function Defense(){
+
+    }
+    public function Keeper(){
+
+    }
+}
